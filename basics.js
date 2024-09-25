825d09ab97b69d6428cf72e706f89206b27bf2fe
Abdalrhman Abuwarda <eng.abdalrhman33@gmail.com>
2024-09-25 12:17:03 +0300

    inilise the basice file

---
+++ basics.js
@@ -0,0 +1,32 @@
+'use strict'
+/*
+let js = 'amazing';
+// if (js === 'amazing') alert("JavaScript is Amazing");
+console.log("Hello World");
+
+let firstName = "Abdalrhman";
+console.log(firstName);
+
+let myFirstJop = "programmer";
+let myCurrentJop = "Teacher";
+
+console.log(typeof true);
+console.log(typeof js);
+console.log(typeof 23);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1990;
+
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+let age = 31;
+age = 30;
+
+const birthYear = 2002;
